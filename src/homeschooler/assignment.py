5f11ba3f4169d6b40653ef03bada79c6c4881141
David Colls <dcolls@dcmbp.local>
2020-07-18 09:09:30 +1000

    add round robin oscillating assignment

--- src/homeschooler/assignment.py
+++ src/homeschooler/assignment.py
@@ -1,3 +1,19 @@
+# heuristic for a good place to start
+def assign_round_robin_oscillating(tasks, n):
+    assignment = [-1] * len(tasks.t)
+    i = 0
+    di = 1
+    for k in tasks.t_efforts_desc:
+        j = tasks.keys_asc.index(k)
+        assignment[j] = i
+        i = i + di
+        if i >= n:
+            i = i - di
+            di = -1
+        if i <= 0:
+            i = i - di
+            di = 1
+    return tuple(assignment)
 
 
 class Assignment:
@@ -9,4 +25,3 @@ class Assignment:
     def neighbours(self, n):
         return [tuple((a + r * (i == j)) % n for i, a in enumerate(self.a))
                 for r in range(1, n) for j, _ in enumerate(self.a)]
-
--- test/testassignment.py
+++ test/testassignment.py
@@ -1,6 +1,7 @@
 import unittest
 
-from homeschooler.assignment import Assignment
+from homeschooler.assignment import Assignment, assign_round_robin_oscillating
+from homeschooler.tasks import Tasks
 
 
 class TestAssignment(unittest.TestCase):
@@ -14,3 +15,7 @@ class TestAssignment(unittest.TestCase):
         self.assertEqual(neighbours[0], (1, 0, 1))
         self.assertEqual(neighbours[1], (0, 1, 1))
         self.assertEqual(neighbours[2], (0, 0, 0))
+
+    def test_assign_round_robin_oscillating(self):
+        a = assign_round_robin_oscillating(Tasks(Tasks.TASKS_2_YES), 2)
+        self.assertEqual(a, (0, 1, 1))
