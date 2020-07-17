91db2411e805a8b63aaba8626d4c772879cbcf3e
David Colls <dcolls@dcmbp.local>
2020-07-18 08:29:42 +1000

    fix syntax tuple generation

--- src/homeschooler/assignment.py
+++ src/homeschooler/assignment.py
@@ -7,6 +7,6 @@ class Assignment:
 
     # Neighbours have just one task assigned to a different child
     def neighbours(self, n):
-        return [tuple([(a + r * (i == j)) % n for i, a in enumerate(self.a)])
+        return [tuple((a + r * (i == j)) % n for i, a in enumerate(self.a))
                 for r in range(1, n) for j, _ in enumerate(self.a)]
 
